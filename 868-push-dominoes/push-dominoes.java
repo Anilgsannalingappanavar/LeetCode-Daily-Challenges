4153b144c14ba7522d689fb72a3ba3e45760962d
Anil G S <anilraj143gs@gmail.com>
2025-05-02 22:30:36 +0530

    Time: 22 ms (50.94%) | Memory: 45.4 MB (91.70%) - LeetSync

---
+++ 868-push-dominoes/push-dominoes.java
@@ -0,0 +1,45 @@
+class Solution {
+    public String pushDominoes(String dominoes) {
+        
+    
+        char[] arr = dominoes.toCharArray();
+        int n = arr.length;
+        int[] forces = new int[n];
+
+        // Force from the right
+        int force = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 'R') {
+                force = n;
+            } else if (arr[i] == 'L') {
+                force = 0;
+            } else {
+                force = Math.max(force - 1, 0);
+            }
+            forces[i] += force;
+        }
+
+        // Force from the left
+        force = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (arr[i] == 'L') {
+                force = n;
+            } else if (arr[i] == 'R') {
+                force = 0;
+            } else {
+                force = Math.max(force - 1, 0);
+            }
+            forces[i] -= force;
+        }
+
+        // Build final result
+        StringBuilder result = new StringBuilder();
+        for (int f : forces) {
+            if (f > 0) result.append('R');
+            else if (f < 0) result.append('L');
+            else result.append('.');
+        }
+
+        return result.toString();
+    }
+}
